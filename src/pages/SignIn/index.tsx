9540bf564b9b1ee3182da165accd9265cf61ae80
gusflopes <gusflopes86@gmail.com>
2020-05-13 21:30:31 -0400

    feat: signIn page layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,82 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+  const [isKeyboardVisible, setIsKeyBoardVisible] = useState(false);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => {
+      setIsKeyBoardVisible(true);
+    });
+    Keyboard.addListener('keyboardDidHide', () => {
+      setIsKeyBoardVisible(false);
+    });
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Faça seu logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-        onPress={() => {
-          console.log('yupi');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('yupi');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!isKeyboardVisible && (
+        <CreateAccountButton onPress={() => {}}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar minha conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
   );
 };
 
